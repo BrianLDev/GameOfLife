84a432747fbd749d2d9584cf8b908d2bf7fb8392
Brian Leip <brianleip@gmail.com>
2020-09-04 02:03:17 -0700

    Created simple sprites. Made Unity 2D tilemap

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Grid {
+
+    private int width, height;
+    private int[,] gridArray;
+
+    public Grid(int w, int h) {
+        width = w;
+        height = h;
+        gridArray = new int[w, h];
+    }
+
+}
