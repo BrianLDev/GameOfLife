5b5e15e1c621f6dbc44cbf8c84f4581c89b8c01d
Brian Leip <brianleip@gmail.com>
2020-09-05 21:28:51 -0700

    Created SmartTile script, started GridManager script. Added Unity Sample UI package and cleaned up their mess into folders

---
+++ Assets/Scripts/Tilemap/GridManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager : MonoBehaviour {
+  public GridLayout grid;
+  public Tilemap tilemap;
+
+  private void Awake() {
+    if(!grid) {
+      grid = GameObject.FindObjectOfType<GridLayout>();
+    }
+    if(!tilemap) {
+      tilemap = GameObject.FindObjectOfType<Tilemap>();
+    }
+  }
+
+  private void Start() {
+    Debug.Log("Size = " + tilemap.size);
+  }
+}
--- Assets/Scripts/Tilemap/tmapTile.cs
+++ Assets/Scripts/Tilemap/tmapTile.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class tmapTile : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[CreateAssetMenu(fileName = "New GOL Tile", menuName = "Tile-GOL")]
+public class TmapTile : Tile {
+
+    public enum TileState { Empty, Outline, Alive };
+    public Sprite spriteEmpty, spriteAlive, spriteOutline;
+    private TileState state = TileState.Empty;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData) {
+        // This is called anytime a Tile is added in Unity Editor or in game
+
+        int i = Random.Range(1, 100);
+        if (i < 33) {
+            state = TileState.Empty;
+            tileData.sprite = spriteEmpty;
+        }
+        else if (i < 66) {
+            state = TileState.Outline;
+            tileData.sprite = spriteOutline;
+        }
+        else {
+            state = TileState.Alive;
+            tileData.sprite = spriteAlive;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go) {
+        // This is called once on first frame rendering
+        // TODO: ADD THINGS HERE IF NEEDED
+        return true;
     }
+
 }
