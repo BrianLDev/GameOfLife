c607326f6708df2b3a0ba7c173c4ad1d7cb1b057
Brian Leip <brianleip@gmail.com>
2020-09-06 01:06:20 -0700

    Expanded max grid size to 500x500 and auto-adjust camera FOV

--- Assets/Scripts/Tilemap/GridManager.cs
+++ Assets/Scripts/Tilemap/GridManager.cs
@@ -28,19 +28,25 @@ public class GridManager : MonoBehaviour {
   }
 
   private void Update() {
-    if ((mainCamera.fieldOfView <= targetFOV*.97) || (mainCamera.fieldOfView >= targetFOV*1.03)) {
-      mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, targetFOV, Time.deltaTime*3.0f);
+    if ((mainCamera.fieldOfView <= targetFOV*.995) || (mainCamera.fieldOfView >= targetFOV*1.005)) {
+      mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, targetFOV, Time.deltaTime*3.5f);
     }
   }
 
   public void SetWidth(int widthIndex) {
-    if(widthIndex==0) { widthIndex = 1; }
-    gridWidth = (widthIndex+1)*5;
+    if (widthIndex==0) { gridWidth = 5; }
+    else if (widthIndex <=19)
+      gridWidth = (widthIndex+1)*5;   // increments of 5 from 5 to 100 (index 0 to 19)
+    else
+      gridWidth = (widthIndex-17)*50; // increments of 50 from 150 to 500 (index 20 to 27)
   }
 
   public void SetHeight(int heightIndex) {
-    if(heightIndex==0) { heightIndex = 1; }
-    gridHeight = (heightIndex+1)*5;
+    if (heightIndex==0) { gridHeight = 5; }
+    else if (heightIndex <= 19)
+      gridHeight = (heightIndex+1)*5;   // increments of 5 from 5 to 100 (index 0 to 19)
+    else if (heightIndex >= 20)
+      gridHeight = (heightIndex-17)*50; // increments of 50 from 150 to 500 (index 20 to 27)
   }
 
   public void CreateGrid() {
@@ -76,10 +82,18 @@ public class GridManager : MonoBehaviour {
     else if(gridWidth <= 60 && gridHeight <= 40)
       targetFOV = 135;
     else if(gridWidth <= 75 && gridHeight <= 50)
-      targetFOV = 145;
-    else if(gridWidth <= 110 && gridHeight <= 75)
-      targetFOV = 155;      
+      targetFOV = 142;
+    else if(gridWidth <= 100 && gridHeight <= 75)
+      targetFOV = 153;      
     else if(gridWidth <= 150 && gridHeight <= 100)
-      targetFOV = 165;  
+      targetFOV = 160;  
+    else if(gridWidth <= 300 && gridHeight <= 200)
+      targetFOV = 170;  
+    else if(gridWidth <= 450 && gridHeight <= 300)
+      targetFOV = 172.5f;  
+    else if(gridWidth <= 600 && gridHeight <= 400)
+      targetFOV = 175.5f;  
+    else if(gridWidth <= 750 && gridHeight <= 500)
+      targetFOV = 176.5f;  
   }
 }
