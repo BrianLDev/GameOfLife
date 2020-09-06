44f8cf6a611ddc01c26e50f039df63a6f4e8924a
Brian Leip <brianleip@gmail.com>
2020-09-05 22:34:21 -0700

    Connected UI dropdowns and button to GridManager

--- Assets/Scripts/Tilemap/GridManager.cs
+++ Assets/Scripts/Tilemap/GridManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.Tilemaps;
 public class GridManager : MonoBehaviour {
   public GridLayout grid;
   public Tilemap tilemap;
+  private int gridWidth, gridHeight;
 
   private void Awake() {
     if(!grid) {
@@ -18,5 +19,18 @@ public class GridManager : MonoBehaviour {
 
   private void Start() {
     Debug.Log("Size = " + tilemap.size);
+  }
+
+  public void SetWidth(int width) {
+    gridWidth = width+1;
+  }
+
+  public void SetHeight(int height) {
+    gridHeight = height+1;
+  }
+
+  public void CreateGrid() {
+    Debug.Log("Creating a " + gridWidth + " x " + gridHeight + " grid.");
+    // TODO: CREATE GRID
   }
 }
